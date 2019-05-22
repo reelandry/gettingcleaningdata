e6346c28ef5130e8c7627d6d74917c738cdcab61
Richard Spencer Landry, Ph.D <34815080+reelandry@users.noreply.github.com>
2019-05-22 15:26:46 -0500

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,67 +1,88 @@
-## R script called run_analysis.R that cleans a number of data sets
-# load the reshape2 package
-library(reshape2)
+##################################################################################################
+## run_analysis.R
+## This R script first downloads the appropriate zip file, "UCIdata.zip", extracts to  
+## the current working directory if it does not already exist, and performs the
+## appropriate analysis to tidy the relevant data to a separate file "tidy_data.txt"  
+##################################################################################################
 
-## 1) Merges the training and the test sets to create one data set.
+##################################################################################################
+## Part 1: Initialization and Labeling
+##################################################################################################
 
-# read data into data frames
-sub_train <- read.table("subject_train.txt")
-sub_test <- read.table("subject_test.txt")
-X_train <- read.table("X_train.txt")
-X_test <- read.table("X_test.txt")
-y_train <- read.table("y_train.txt")
-y_test <- read.table("y_test.txt")
+# Declare string variables for file download.
+fileName <- "UCIdata.zip"
+url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+dir <- "UCI HAR Dataset"
 
-# column names for subject files
-names(sub_train) <- "subjectID"
-names(sub_test) <- "subjectID"
+# Determine file size for preprocessing.
+response = httr::HEAD(url)
+print(as.numeric(httr::headers(response)[["Content-Length"]])/(10^6))
 
-# column names for measurement files
-featureNames <- read.table("features.txt")
-names(X_train) <- featureNames$V2
-names(X_test) <- featureNames$V2
+# If file does not exist, download to working directory.
+if(!file.exists(fileName)){
+    download.file(url,fileName, mode = "wb") 
+}
 
-# column names for label files
-names(y_train) <- "activity"
-names(y_test) <- "activity"
+# File unzip verification. If the directory does not exist, unzip the downloaded file.
+if(!file.exists(dir)){
+    unzip("UCIdata.zip", files = NULL, exdir=".")
+}
 
-# combine files into one dataset
-train <- cbind(sub_train, y_train, X_train)
-test <- cbind(sub_test, y_test, X_test)
-combined <- rbind(train, test)
+# Read data from the required files and set to internal R variables.
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+features <- read.table("UCI HAR Dataset/features.txt")  
 
-## 2) Extracts only the measurements on the mean and standard
-## deviation for each measurement.
+##################################################################################################
+## Part 2: Analysis
+##################################################################################################
 
-# determine which columns contain "mean()" or "std()"
-meanstdcols <- grepl("mean\\(\\)", names(combined)) |
-    grepl("std\\(\\)", names(combined))
+# 1) Merges the training and the test sets to create one data set.
+dataSet <- rbind(X_train,X_test)
 
-# ensure that we also keep the subjectID and activity columns
-meanstdcols[1:2] <- TRUE
+# 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Create a vector of only mean and std, use the vector to subset.
+MeanStdOnly <- grep("mean()|std()", features[, 2]) 
+dataSet <- dataSet[,MeanStdOnly]
 
-# remove unnecessary columns
-combined <- combined[, meanstdcols]
+# 4) Appropriately labels the data set with descriptive activity names. Create vector of 
+# "Clean" feature names by getting rid of "()" apply to the dataSet to rename labels.
+CleanFeatureNames <- sapply(features[, 2], function(x) {gsub("[()]", "",x)})
+names(dataSet) <- CleanFeatureNames[MeanStdOnly]
 
-## 3) Uses descriptive activity names to name the activities
-## in the data set, and 4) Appropriately labels the data set with descriptive
-## activity names.
+# Combine test and train of subject data and activity data, give descriptive labels.
+subject <- rbind(subject_train, subject_test)
+names(subject) <- 'subject'
+activity <- rbind(y_train, y_test)
+names(activity) <- 'activity'
 
-# convert the activity column from integer to factor
-combined$activity <- factor(combined$activity, labels=c("Walking",
-                                                        "Walking Upstairs",
-                                                        "Walking Downstairs",
-                                                        "Sitting",
-                                                        "Standing",
-                                                        "Laying"))
+# Combine subject, activity, and mean and std only data set to create final data set.
+dataSet <- cbind(subject,activity, dataSet)
 
+# 3) Uses descriptive activity names to name the activities in the data set. Group the
+# activity column of dataSet, re-name label of levels with activity_levels and apply it to
+# dataSet.
+act_group <- factor(dataSet$activity)
+levels(act_group) <- activity_labels[,2]
+dataSet$activity <- act_group
 
-## 5) Creates a second, independent tidy data set with the
-## average of each variable for each activity and each subject.
+# 5) Creates a second, independent tidy data set with the average of each variable for
+# each activity and each subject. 
 
-# create the tidy data set
-melted <- melt(combined, id=c("subjectID","activity"))
-tidy <- dcast(melted, subjectID+activity ~ variable, mean)
+# Check if reshape2 package is installed.
+if (!"reshape2" %in% installed.packages()) {
+    install.packages("reshape2")
+}
+library("reshape2")
 
-# write the tidy data set to a file
-write.csv(tidy, "tidy.csv", row.names=FALSE)
+# melt data to tall skinny data and cast means. Finally write the tidy data to the
+# working directory as "tidy_data.txt".
+baseData <- melt(dataSet,(id.vars=c("subject","activity")))
+secondDataSet <- dcast(baseData, subject + activity ~ variable, mean)
+names(secondDataSet)[-c(1:2)] <- paste("[mean of]" , names(secondDataSet)[-c(1:2)] )
+write.table(secondDataSet, "tidy_data.txt", sep = ",", row.name=FALSE)
